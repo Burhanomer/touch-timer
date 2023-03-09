196e2403dbf93e90ca9493bc4ae620e8520d0b8d
Burhanomer <125546219+Burhanomer@users.noreply.github.com>
2023-03-09 09:47:31 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
-let Start = 0
 let Time = 0
-input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    Start = input.runningTime()
-    basic.showIcon(IconNames.Happy)
-})
+let Start = 0
 input.onLogoEvent(TouchButtonEvent.Released, function () {
     Time = input.runningTime() - Start
-    basic.showNumber(Time / 1000)
+    basic.showNumber(Math.idiv(input.runningTime(), 1000))
+    basic.pause(1000)
+    basic.showNumber(Time % 1000)
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    Start = input.runningTime()
+    basic.showIcon(IconNames.Happy)
 })
